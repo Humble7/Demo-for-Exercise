3b73d60746ed086a57a493b9f8be10d89debbd59
ChenZhen <chenzhennba@gmail.com>
2019-06-03 16:43:58 +0800

    New Project

---
+++ BlockExercise/BlockExercise/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BlockExercise
+//
+//  Created by ChenZhen on 2019/6/3.
+//  Copyright © 2019 ChenZhen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
